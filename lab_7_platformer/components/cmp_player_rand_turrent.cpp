42a5f55139214211c9036a222bafa16ceb7107a2
Denis Flynn <denflynn@gmail.com>
2022-12-05 09:10:53 +0000

    Fixed link issue. Reload cmake is all. CMakeLists written to include new files. Runs but is not useful or interesting.

--- lab_7_platformer/components/cmp_player_rand_turrent.cpp
+++ lab_7_platformer/components/cmp_player_rand_turrent.cpp
@@ -15,20 +15,20 @@ void PlayerRandTurretComponent::update(double dt) {
   static float angle = 0.f;
   angle += 1.f * dt;
 
-  _parent->setRotation(180.f + sin(angle) * 45.f);
+  //_parent->setRotation(180.f + sin(angle) * 45.f);// leave out for now. May place shape on top of player that will rotate.
 }
 
 void PlayerRandTurretComponent::fire() const {
   auto bullet = _parent->scene->makeEntity();
   bullet->setPosition(_parent->getPosition());
-  bullet->addComponent<HurtComponent>();
+  //bullet->addComponent<HurtComponent>(); //harmless bullet. blank?
   bullet->addComponent<BulletComponent>();
   auto s = bullet->addComponent<ShapeComponent>();
 
   s->setShape<sf::CircleShape>(8.f);
   s->getShape().setFillColor(Color::Red);
   s->getShape().setOrigin(Vector2f(8.f, 8.f));
-  auto p = bullet->addComponent<PhysicsComponent>(true, Vector2f(8.f, 8.f));
+  auto p = bullet->addComponent<PhysicsComponent>(true, Vector2f(1.f, 1.f));//(8.f, 8.f));
   p->setRestitution(.4f);
   p->setFriction(.005f);
   p->impulse(sf::rotate(Vector2f(0, 15.f), -_parent->getRotation()));
